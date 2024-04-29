7a14fda278e8f206b90aadab1aceecd5afa75e95
Tomás Montoya <tomas1213monto@gmail.com>
2024-04-29 08:05:56 -0500

    First Commit

---
+++ src/main/java/com/muvit/MUVIT/MuvitApplication.java
@@ -0,0 +1,13 @@
+package com.muvit.MUVIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuvitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuvitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muvit/MUVIT/MuvitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muvit.MUVIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuvitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
