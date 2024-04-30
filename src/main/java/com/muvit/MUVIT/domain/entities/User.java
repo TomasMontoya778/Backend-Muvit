c7e5b38aa96c1e0dbd222d6538a725fa92b8a928
HernanDavidA <morenoalvare123@gmail.com>
2024-04-30 07:28:04 -0500

    Creation of user entity

---
+++ src/main/java/com/muvit/MUVIT/domain/entities/User.java
@@ -0,0 +1,28 @@
+package com.muvit.MUVIT.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(length = 20, nullable = false)
+    private String name;
+    @Column(length = 20, nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+
+    private Long id_rol;
+}
