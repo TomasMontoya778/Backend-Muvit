9982290c04174c23e17cecde9a8214548b226124
HernanDavidA <morenoalvare123@gmail.com>
2024-05-02 07:44:52 -0500

    feature/creating-domain-repositories

---
+++ src/main/java/com/muvit/MUVIT/domain/repositories/RolRepository.java
@@ -0,0 +1,9 @@
+package com.muvit.MUVIT.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.muvit.MUVIT.domain.entities.Rol;
+
+public interface RolRepository extends JpaRepository<Rol, Long>{
+
+}
