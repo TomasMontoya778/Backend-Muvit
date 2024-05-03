2016859277923366fb385409c25bd0f0b491d5bd
Samuel Rodriguez <samuelrodriguez795@gmail.com>
2024-05-03 07:17:10 -0500

    feature/create-controllers

---
+++ src/main/java/com/muvit/MUVIT/api/controllers/RolControlle.java
@@ -0,0 +1,63 @@
+package com.muvit.MUVIT.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.muvit.MUVIT.api.dto.request.RolRequest;
+import com.muvit.MUVIT.api.dto.response.RolResponse;
+import com.muvit.MUVIT.infrastructure.abstract_services.interfaces.IRolService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/rol")
+@AllArgsConstructor
+public class RolControlle {
+    @Autowired
+    private final IRolService rolService;
+        @GetMapping
+    public ResponseEntity<Page<RolResponse>> get(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "2") int size) {
+
+        return ResponseEntity.ok(this.rolService.getAll(page - 1, size));
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<RolResponse> getById(
+            @PathVariable String id) {
+        return ResponseEntity.ok(this.rolService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<RolResponse> insert(
+            @Validated @RequestBody RolRequest company) {
+        return ResponseEntity.ok(this.rolService.create(company));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        this.rolService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<RolResponse> update(
+            @PathVariable String id, // id por url
+            @Validated @RequestBody RolRequest company // compañia actualizada
+    ) {
+        return ResponseEntity.ok(this.rolService.update(id, company));
+    }
+}
