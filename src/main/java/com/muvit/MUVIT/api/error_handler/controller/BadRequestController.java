1aab01d6702746bbad26bf4bf328ebec8c35adcd
Samuel Rodriguez <samuelrodriguez795@gmail.com>
2024-05-03 07:49:56 -0500

    origin/feature/create-badRequestController

---
+++ src/main/java/com/muvit/MUVIT/api/error_handler/controller/BadRequestController.java
@@ -0,0 +1,51 @@
+package com.muvit.MUVIT.api.error_handler.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.muvit.MUVIT.api.error_handler.response.BaseErrorResponse;
+import com.muvit.MUVIT.api.error_handler.response.ErrorResponse;
+import com.muvit.MUVIT.api.error_handler.response.ErrorsResponse;
+import com.muvit.MUVIT.util.exceptions.IdNotFoundException;
+
+
+/*
+ * RestControllerAdvice = Controlador de errores 
+ */
+@RestControllerAdvice
+/**
+ * Status de error del controlador
+ */
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class BadRequestController {
+
+    @ExceptionHandler(IdNotFoundException.class)
+    public BaseErrorResponse handleIdNotFound(IdNotFoundException exception) {
+
+        return ErrorResponse.builder()
+                .code(HttpStatus.BAD_REQUEST.value())
+                .status(HttpStatus.BAD_REQUEST.name())
+                .message(exception.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseErrorResponse handleErrors(MethodArgumentNotValidException exception) {
+
+        List<String> errors = new ArrayList<>();
+
+        exception.getAllErrors().forEach(error -> errors.add(error.getDefaultMessage()));
+
+        return ErrorsResponse.builder()
+                .code(HttpStatus.BAD_REQUEST.value())
+                .status(HttpStatus.BAD_REQUEST.name())
+                .errors(errors)
+                .build();
+    }
+}
